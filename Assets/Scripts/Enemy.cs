4ed057c4ae54f1b2836892d7ef08eec03a732932
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2024-09-25 10:52:07 +0100

    Minor code cleanup

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using PrimeTween;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Splines;
+using Random = UnityEngine.Random;
 
 [Serializable]
 public class Enemy : MonoBehaviour
@@ -12,7 +14,7 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] private float _moveSpeed = 1.5f;
 
-    private float _progress;
+    private float _distanceTravelled;
 
     private void Awake()
     {
@@ -27,22 +29,26 @@ public class Enemy : MonoBehaviour
     private void LateUpdate()
     {
         var originalPosition = transform.position;
-        
-        _progress += _moveSpeed * Time.deltaTime;
-        var splineProgress = _progress / _path.Spline.GetLength();
 
-        var splinePosition = _path.Spline.EvaluatePosition(splineProgress);
-        transform.position = _path.transform.TransformPoint(splinePosition);
+        _distanceTravelled += _moveSpeed * Time.deltaTime;
+        var progress = _distanceTravelled / _path.Spline.GetLength();
+
+        transform.position = _path.transform.TransformPoint(_path.Spline.EvaluatePosition(progress));
 
         var direction = (transform.position - originalPosition).normalized;
         transform.up = -direction;
-        
-        if (splineProgress >= 1f)
+
+        if (progress >= 1f)
         {
             GameManager.Instance.HitBase();
             Tween.Scale(transform, 0f, 0.5f, Ease.InBack).OnComplete(() => Destroy(gameObject));
 
             enabled = false;
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(transform.position, transform.position + transform.right, Color.yellow);
     }
 }
