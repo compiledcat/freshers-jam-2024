92d08afd5abab66c6880a257629103fff20dae9e
Serpendio <58090288+Serpendio@users.noreply.github.com>
2024-09-30 01:05:31 +0100

    Including the Congrats scene & frog domination

--- Assets/Scripts/WorldMap.cs
+++ Assets/Scripts/WorldMap.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class WorldMap : MonoBehaviour
 {
@@ -18,22 +19,42 @@ public class WorldMap : MonoBehaviour
     }
 
     [SerializeField] Camera cam;
-    [SerializeField] GameObject ending;
+    [SerializeField] GameObject congrats, ending, returnBtn, frog;
     [SerializeField] List<Positions> scenePositions;
     [SerializeField] private float cameraSpeed;
+    [SerializeField] private int numFrogs;
 
 
     private void Awake()
     {
         if (GameManager.Instance.currentLevel == 2)
         {
-            ending.SetActive(true);
-            //Tween.Scale(ending.transform, Vector3.one * .8f, 1f, ease: Ease.InOutCubic);
+            StartCoroutine(PlayEnding());
             return;
         }
 
         LerpCam(GameManager.Instance.currentLevel + 1, GameManager.Instance.currentLevel + 2);
         GameManager.Instance.currentLevel++;
+    }
+
+    private IEnumerator PlayEnding()
+    {
+        LerpCam(GameManager.Instance.currentLevel + 1, 0);
+        yield return new WaitForSeconds(Vector3.Distance(scenePositions[GameManager.Instance.currentLevel + 1].position.position, scenePositions[0].position.position) / cameraSpeed);
+        float timeToWait = 4f;
+        for (int i = 0; i < numFrogs; i++)
+        {
+            GameObject newFrog = Instantiate(frog, new Vector3(Random.Range(-scenePositions[0].maxSize.x, scenePositions[0].maxSize.x), Random.Range(-scenePositions[0].maxSize.y, scenePositions[0].maxSize.y), -.1f), Quaternion.Euler(0, 0, Random.Range(-10, 10)));
+            newFrog.transform.localScale *= Random.Range(.25f, .4f);
+            yield return new WaitForSeconds(timeToWait);
+            timeToWait *= .8f;
+        }
+        yield return new WaitForSeconds(.5f);
+        ending.SetActive(true);
+        yield return new WaitForSeconds(10f);
+        congrats.SetActive(true);
+        Tween.Scale(returnBtn.transform, .76f, .84f, 4, Ease.Linear, cycles: -1, cycleMode: CycleMode.Rewind);
+        Tween.LocalRotation(returnBtn.transform, Vector3.forward * -10, Vector3.forward * 10, 4, Ease.Linear, cycles: -1, cycleMode: CycleMode.Rewind);
     }
 
     public void MainMenu()
@@ -53,7 +74,7 @@ public class WorldMap : MonoBehaviour
         }*/
         float advancementTimeForNextScene = Vector3.Distance(scenePositions[start].position.position, scenePositions[end].position.position) / cameraSpeed;
         //cam.orthographicSize = Mathf.Lerp(scenePositions[start].maxSize.y, scenePositions[end].maxSize.y, t);
-        transform.position = scenePositions[start].position.position;
+        cam.transform.position = scenePositions[start].position.position;
         Sequence.Create()
             .Group(Tween.Position(cam.transform, scenePositions[start].position.position - Vector3.forward * 10, scenePositions[0].position.position - Vector3.forward * 10, advancementTimeForNextScene, ease: Ease.InOutCubic))
             .Group(Tween.CameraOrthographicSize(cam, scenePositions[start].maxSize.y, scenePositions[0].maxSize.y, advancementTimeForNextScene, ease: Ease.InOutCubic))
