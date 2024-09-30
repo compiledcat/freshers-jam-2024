653343aaf59b21b74619c9ea27ef34409fe15627
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2024-09-30 19:53:33 +0100

    New water tile + remove unnecessary spline(?)

--- Assets/Scripts/WaterTiler.cs
+++ Assets/Scripts/WaterTiler.cs
@@ -1,28 +1,39 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class WaterTiler : MonoBehaviour
 {
     [SerializeField] private RawImage _rawImage;
+    [ShowInInspector] [ReadOnly] private float _textureAspect;
+
     private Camera _cam;
-    private float _speed = 0.05f;
+
+    [SerializeField] private float _speed = 0.05f;
+    [SerializeField] private float _scaleFactor = 1;
 
     private void Awake()
     {
         _cam = Camera.main;
+        _textureAspect = _rawImage.texture.width / (float)_rawImage.texture.height;
     }
-    
+
     private void Update()
     {
-        _rawImage.rectTransform.sizeDelta = new Vector2(_cam.orthographicSize * 2 * _cam.aspect, _cam.orthographicSize * 2);
-        
         // offset over time
         var offset = _rawImage.uvRect.position;
         offset.x += Time.deltaTime * _speed;
         offset.y += Time.deltaTime * _speed;
-        _rawImage.uvRect = new Rect(offset, _rawImage.uvRect.size);
+
+        // ensure texture isn't stretched
+        var aspect = _cam.aspect / _textureAspect;
+
+        _rawImage.uvRect = new Rect(
+            offset,
+            new Vector2(_cam.orthographicSize * aspect, _cam.orthographicSize) * _scaleFactor
+        );
     }
-    
+
     private void OnValidate()
     {
         if (_rawImage == null)
