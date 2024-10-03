e8de604e0336df5a623e6dda8a7aa7c9de73fe35
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2024-10-03 19:02:30 +0100

    Fix scaling of some UI and VS screen

--- Assets/Scripts/VS_Screen.cs
+++ Assets/Scripts/VS_Screen.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 
 public class VS_Screen : MonoBehaviour
 {
-    private readonly Vector2 direction = new(1902, 1074);
+    // private readonly Vector2 direction = new(1902, 1074);
     [SerializeField] float time = 4, delay = 4;
     [SerializeField] private bool inverse;
 
     private void Awake()
     {
+        var startPos = new Vector2(-1920, -1080);
+        startPos *= inverse ? -1 : 1;
+        transform.position = startPos;
+        
         Sequence.Create()
-            .Chain(Tween.Position(transform, transform.position - (Vector3)direction.normalized * ((RectTransform)transform).sizeDelta.y * (inverse ? -2.5f : 2.5f), transform.position, time, Ease.OutQuad))
+            .Chain(Tween.LocalPosition(transform, Vector3.zero, time, Ease.OutQuad))
             .ChainDelay(delay)
-            .Chain(Tween.Position(transform, transform.position, transform.position + (Vector3)direction.normalized * ((RectTransform)transform).sizeDelta.y * (inverse ? -2.5f : 2.5f), time, Ease.InQuad));
+            .Chain(Tween.LocalPosition(transform, startPos * -1, time, Ease.InQuad));
+        
+        // Sequence.Create()
+        //     .Chain(Tween.Position(transform, transform.position - (Vector3)direction.normalized * ((RectTransform)transform).sizeDelta.y * (inverse ? -2.5f : 2.5f), transform.position, time, Ease.OutQuad))
+        //     .ChainDelay(delay)
+        //     .Chain(Tween.Position(transform, transform.position, transform.position + (Vector3)direction.normalized * ((RectTransform)transform).sizeDelta.y * (inverse ? -2.5f : 2.5f), time, Ease.InQuad));
     }
 }
